a6b8f86892b3144e9f77b4f488592a2459b949b5
goesma <esmagoktekin01@gmail.com>
2020-03-12 07:49:43 +0300

    Create folder structure.

---
+++ Demo500px/AppDelegate/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Demo500px
+//
+//  Created by EsmaGO on 3/12/20.
+//  Copyright © 2020 EsmaGO. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+

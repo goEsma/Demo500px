f521d880ea3de74f0ae3bf0db6127555e759fcd2
goesma <esmagoktekin01@gmail.com>
2020-03-22 23:03:35 +0300

    Fix sizing cells.

--- Demo500px/Scenes/PopularPhotos/PopularPhotosCollectionViewController.swift
+++ Demo500px/Scenes/PopularPhotos/PopularPhotosCollectionViewController.swift
@@ -24,14 +24,18 @@ class PopularPhotosCollectionViewController: UICollectionViewController {
     
     var photos:[PhotosModel.ViewModel.PhotoViewModel] = [] {
         didSet {
+            self.cellSizes = self.getCellSizes(for: photos)
+        }
+    }
+    private var cellSizes:[CGSize] = [] {
+        didSet {
             self.collectionView.reloadData()
         }
     }
-    
     private let reuseIdentifier = "PhotoCollectionViewCell"
     private let headerResuseIdentifier = "PhotoHeaderCollectionViewCell"
-
-
+    
+    
     // MARK: - Object lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,7 +44,7 @@ class PopularPhotosCollectionViewController: UICollectionViewController {
     }
     
     // MARK: - Helpers
-
+    
     /// Adds activity indicator on collectionView.
     private func addIndicator() {
         let indicator = UIActivityIndicatorView(style: .whiteLarge)
@@ -50,12 +54,48 @@ class PopularPhotosCollectionViewController: UICollectionViewController {
         collectionView.backgroundView = indicator
     }
     
+    private func getCellSizes(for photos: [PhotosModel.ViewModel.PhotoViewModel]) -> [CGSize] {
+        var i = 0
+        var sizes:[CGSize] = []
+        
+        let fullWidth = self.view.frame.size.width - 16
+        
+        while i < photos.count - 1 {
+            let size1 = CGSize(width: CGFloat(photos[i].width),
+                               height: CGFloat(photos[i].height))
+            let size2 = CGSize(width: CGFloat(photos[i+1].width),
+                               height: CGFloat(photos[i+1].height))
+            
+            let ratio1 = size1.width / size1.height
+            let ratio2 = size2.width / size2.height
+            
+            let width1 = fullWidth*ratio1/(ratio1+ratio2)
+            let width2 = fullWidth*ratio2/(ratio1+ratio2)
+            let height = fullWidth/(ratio1+ratio2)
+            
+            let newSize1 = CGSize(width: width1, height: height)
+            let newSize2 = CGSize(width: width2, height: height)
+            
+            sizes.append(newSize1)
+            sizes.append(newSize2)
+            
+            i += 2
+        }
+        if photos.count % 2 != 0 {
+            guard let photo = photos.last else { return sizes }
+            let height = fullWidth * CGFloat(photo.height) / CGFloat(photo.width)
+            let size = CGSize(width: fullWidth, height: height)
+            sizes.append(size)
+        }
+        return sizes
+    }
+    
     // MARK: - UICollectionViewDataSource
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 2
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
         if section == 0 {
@@ -63,7 +103,7 @@ class PopularPhotosCollectionViewController: UICollectionViewController {
         }
         return photos.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if indexPath.section == 0 {
             if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: headerResuseIdentifier, for: indexPath) as? PhotoHeaderCollectionViewCell  {
@@ -73,39 +113,33 @@ class PopularPhotosCollectionViewController: UICollectionViewController {
             }
         } else {
             if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? PhotoCollectionViewCell  {
-                     cell.setPhoto(photos[indexPath.row].imagePath)
-                     return cell
+                cell.setPhoto(photos[indexPath.row].imagePath)
+                return cell
             }
         }
-
+        
         return UICollectionViewCell()
     }
-
+    
     // MARK: - UICollectionViewDelegate
-
+    
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         router?.showPhotoDetail(photos[indexPath.row] )
     }
-
+    
 }
 
 extension PopularPhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let photoWidth = photos[indexPath.row].width
-        let photoHeigth = photos[indexPath.row].height
-
-        let ratio:CGFloat = CGFloat(photoHeigth) / CGFloat(photoWidth)
-        let width = self.view.frame.width
-        let height = width * ratio
-
-        if indexPath.section == 0 {
-             return CGSize(width: width, height: 370)
-        } else {
-             return CGSize(width: width, height: height)
-        }
         
+        if indexPath.section == 0 {
+            let fullWidth = self.view.frame.size.width
+            return CGSize(width: fullWidth, height: 370)
+        } else {
+            return cellSizes[indexPath.row]
+        }
     }
-
+    
 }
 
 extension PopularPhotosCollectionViewController: PopularPhotosDisplayLogic {
